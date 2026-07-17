06b6d54bfacada1028468bff8dced4961ff6079b
Poojitha Akurathi <poojithaags@gmail.com>
2026-07-17 05:31:57 +0530

    Delete Backend directory

--- Backend/medicalstoremanagement/src/main/java/com/medicalstore/exception/InsufficientStockException.java
+++
@@ -1,9 +0,0 @@
-package com.medicalstore.exception;
-
-public class InsufficientStockException extends RuntimeException {
-
-	public InsufficientStockException(String message) {
-		super(message);
-	}
-
-}
--- Backend/medicalstoremanagement/src/main/java/com/medicalstore/exception/MedicineNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.medicalstore.exception;
-
-public class MedicineNotFoundException extends RuntimeException {
-
-	public MedicineNotFoundException(String message) {
-		super(message);
-	}
-
-}
